34865fa8d5aa5bdb84c7781a2ea576900879b5d4
yangm <yangm>
2023-03-15 10:37:56 +0800

    1. 统一编码格式。 2. DEVEUI、APPKEY重新生成 3. class.c使用原classC.c文件

--- src/main.c
+++ src/main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-
 #include "delay.h"
 #include "timer.h"
 #include "radio.h"
-#include "rtc-board.h"
-#include "tremo_delay.h"
-#include "tremo_gpio.h"
-#include "tremo_pwr.h"
-#include "tremo_rcc.h"
 #include "tremo_uart.h"
+#include "tremo_gpio.h"
+#include "tremo_rcc.h"
+#include "tremo_pwr.h"
+#include "tremo_delay.h"
+#include "rtc-board.h"
 
 extern int app_start(void);
+
+void uart_log_init(void) {
+  // uart0
+  gpio_set_iomux(GPIOB, GPIO_PIN_0, 1);
+  gpio_set_iomux(GPIOB, GPIO_PIN_1, 1);
+
+  /* uart config struct init */
+  uart_config_t uart_config;
+  uart_config_init(&uart_config);
+
+  uart_config.baudrate = UART_BAUDRATE_115200;
+  uart_init(CONFIG_DEBUG_UART, &uart_config);
+  uart_cmd(CONFIG_DEBUG_UART, ENABLE);
+}
 
 void board_init() {
   rcc_enable_oscillator(RCC_OSC_XO32K, true);
@@ -29,7 +42,7 @@ void board_init() {
   delay_ms(100);
   pwr_xo32k_lpm_cmd(true);
 
-  // uart_log_init();
+  uart_log_init();
 
   RtcInit();
 }
@@ -37,5 +50,16 @@ void board_init() {
 int main(void) {
   // Target board initialization
   board_init();
+
   app_start();
 }
+
+#ifdef USE_FULL_ASSERT
+void assert_failed(void *file, uint32_t line) {
+  (void)file;
+  (void)line;
+
+  while (1) {
+  }
+}
+#endif
